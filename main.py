9c5f7ec0dbb650f94d358c4536ae60b16d827ac9
vnsabi <vanessabii291201@gmail.com>
2022-06-12 16:56:33 +0700

    finish

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from typing import Union
 from fastapi import FastAPI
-
 # import tensorflow
+import json
 
 app = FastAPI()
 
+# model = tensorflow.keras.models.load_model('model.h5')
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Welcome to restabot API"}
 
+@app.get("/api/v1/restabot/data")
+def get_message(message: Union[str, None] = None):
+    # Opening JSON file
+    f = open('data.json')
+    
+    # returns JSON object as 
+    # a dictionary
+    json_file = json.load(f)
+    data = json_file["intents"]
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    lowerCaseMessage = message.lower()
+    for obj in data:
+        inputs = obj["inputs"]
+        response = obj["responses"][0]
+        print(inputs)
+        if lowerCaseMessage in inputs:
+            return { "cnt": response }
